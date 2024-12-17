72e07b8457ef82db47e50bdf394a787190d64888
Bekislam09 <email@example.com>
2024-12-17 21:41:27 +0600

    x

--- src/Home.jsx
+++ src/Home.jsx
@@ -13,53 +13,53 @@ const Home = () => {
                     <Link to={'/Grandfather'} >
                     <img className='img-grandfather' src={black} alt="" />
                     </Link>
-                    <h4>Абдылда</h4>
+                    <h4 className='z' >Абдылда</h4>
                 </div> 
 
                 <div className='grandmother' >
                     <Link to={'/Grandmother'} >
                     <img className='img-grandmother' src={black} alt="" />
                     </Link>
-                    <h4>Токжан</h4>
+                    <h4 className='x' >Токжан</h4>
                 </div>
 
                 <div className='father' >
                     <Link to={'/Father'}>
                     <img className='img-father' src={black} alt=""/>
                     </Link>
-                    <h4>Мурсалбек</h4>
+                    <h4 className='z' >Мурсалбек</h4>
                 </div>
 
                 <div className='mother' >
                     <Link to={'/Mother'} >
                     <img className='img-mother' src={black} alt="" />
                     </Link>
-                    <h4>Батыргул</h4>
+                    <h4 className='x' >Батыргул</h4>
                 </div>
                 <div className='bekislam'>
                     <Link to={'/Bekislam'}>
                     <img className='img-bekislam'  src={black} alt=""/>
                     </Link>
-                    <h4>Бекислам</h4>
+                    <h4 className='z' >Бекислам</h4>
                 </div>
                 <div className='indira' >
                     <Link to={'/Indira'} >
                     <img className='img-indira' src={black} alt="" />
                     </Link>
-                    <h4>Индира</h4>
+                    <h4 className='x' >Индира</h4>
                 </div>
 
                 <div className='bayastan' >
                     <Link to={'/Bayastan'} >
                     <img className='img-bayastan' src={black} alt="" />
                     </Link>
-                    <h4>Баястан</h4>
+                    <h4 className='z' >Баястан</h4>
                 </div>
                 <div className='sabina' >
                     <Link to={'/Sabina'} >
                     <img className='img-sabina' src={black} alt="" />
                     </Link>
-                    <h4>Сабина</h4>
+                    <h4 className='x' >Сабина</h4>
                 </div>
             </div>
         </div>
